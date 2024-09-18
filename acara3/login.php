ad18703923919ff9c6326610a8e9671771e6be4e
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-09-18 09:51:11 +0700

    Create login.php

---
+++ acara3/login.php
@@ -0,0 +1,11 @@
+<?php
+$username = "shinta";
+$password = "123";
+$input_username = "shinta";
+$input_password = "123";
+if ($input_username === $username && $input_password === $password) {
+    echo "Login berhasil!";
+} else {
+    echo "Username atau password yang anda masukkan salah!";
+}
+?>
