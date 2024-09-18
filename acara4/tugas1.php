c676dde4b25a1dbb68b561cb82a915a719d5aa5f
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-09-18 07:35:09 +0700

    Create tugas1.php

---
+++ acara4/tugas1.php
@@ -0,0 +1,6 @@
+<?php
+// $i = 100 dan akan terus berlanjut hingga $i mencapai 1000, dengan setiap iterasi menambah nilai $i sebesar 1
+for ($i = 100; $i <= 1000; $i++) { 
+    echo $i . "\n"; /
+}
+?>
